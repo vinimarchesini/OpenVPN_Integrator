4dc2e950fb25cbc6f066f1d3ae7367838b6ef05d
vinimarchesini <95715554+vinimarchesini@users.noreply.github.com>
2021-12-07 17:27:56 -0300

    A simple integrator with parameters
    Usage:
    Call the exe using cmd and parameters:
                [Option('f', "file", Required = true,
                [Option('c', "config", Required = true,
                [Option('l', "log", Required = true,
                [Option('u', "username", Required = true,
                [Option('p', "password", Required = true,

---
+++ ovpnADM/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommandLine;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ovpnADM
+{
+
+    class Program
+    {
+
+        class Options
+        {
+            [Option('f', "file", Required = true,
+  HelpText = "Coloque o caminho do exe do OpenVPN.")]
+            public string pathfile { get; set; }
+
+            [Option('c', "config", Required = true,
+  HelpText = "Coloque o caminho do config do OpenVPN.")]
+            public string config { get; set; }
+
+            [Option('l', "log", Required = true,
+  HelpText = "Coloque o caminho do log do OpenVPN.")]
+            public string log { get; set; }
+
+            [Option('u', "username", Required = true,
+  HelpText = "Coloque o usuario do OpenVPN.")]
+            public string username { get; set; }
+
+            [Option('p', "password", Required = true,
+  HelpText = "Coloque a senha do OpenVPN.")]
+            public string password { get; set; }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+              .WithParsed(RunOptions)
+              .WithNotParsed(HandleParseError);
+            //Console.ReadKey();
+        }
+        static void RunOptions(Options opts)
+        {
+            //handle options
+            StartOVPN sovpn = new StartOVPN();
+            sovpn.InitSetup(opts.pathfile, opts.config, opts.log, opts.username, opts.password);
+        }
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+            //handle errors
+        }
+    }
+
+}
---
+++ ovpnADM/StartOVPN.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ovpnADM
+{
+    class StartOVPN
+    {
+        public void InitSetup (string pathFile, string args, string log, string username, string password)
+        {
+            ProcessStartInfo ovpnsi = ovpnStartInfo(pathFile, args, log);
+            Start(ovpnsi, username, password);
+        }
+        public ProcessStartInfo ovpnStartInfo(string pathFile, string args, string log)
+        {
+            ProcessStartInfo ovpnsi = new ProcessStartInfo()
+            {
+                FileName = pathFile,
+                Arguments = @"--config """ + args + @""" " + @"--log """ + log + @"""",
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+                Verb = "runas"
+            };
+            return ovpnsi;
+        }
+
+        public void Start(ProcessStartInfo openvpnInfo, string username, string password)
+        {
+            Process openvpn = Process.Start(openvpnInfo);
+            Thread.Sleep(1000);
+            openvpn.StandardInput.WriteLine(username);
+            Thread.Sleep(1000);
+            openvpn.StandardInput.WriteLine(password);
+        }
+    }
+}
